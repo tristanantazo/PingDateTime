066b570d1b4ade08b4096005a5f6a4f17edba554
Tristan Viel Antazo <tristanvielantazo@Tristans-MacBook-Pro.local>
2023-03-08 10:03:56 +0800

    preparation

---
+++ client/src/vite.config.js
@@ -0,0 +1,20 @@
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: true,
+        hmr: {
+            host: "localhost",
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
+});
